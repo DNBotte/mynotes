2d2ab5cc0363abde16b14648b84101186c3906b2
David Botte <davidnbotte@yahoo.com>
2018-10-05 17:30:46 -0400

    Create examStatsPy2.py
    Head start on this exam calculator - finish at home on phone

---
+++ python/codeCademy/play/examStatsPy2.py
@@ -0,0 +1,15 @@
+# An attempt to rewrite an exam statistics analyzer for Python 2.7 written back in the teacher days
+# done from memory for now, then googling later to get it right
+# A version for Python 3 will also be made
+
+# finish this at home on phone
+
+def exmStats(vals):
+    lenny = len(val)
+    total = 0
+    for val in vals:
+        total += val
+    avg = float(total/val)
+    
+    print "Exam results: " + "Average: " + str(avg) + " Lowest: " + str(min(vals)) + " Highest: " + str(max(vals)) + " Median: " + 
+        
