2cf6391ba1928c3c35928ed81edbe01b9b65e1ac
David Botte <davidnbotte@yahoo.com>
2019-02-12 19:47:41 -0500

    Create listScript.js

---
+++ mySite/ideas/invest/listScript.js
@@ -0,0 +1,20 @@
+var titles = ["Combo Funds", "AI", "Fusion Electricity", "Additive Manufacturing", "Asteroid Mining", "Space Transportation", "Desalinization", "Invasive Species", "Lab Grown Agriculture", "3-D Transportation", "Regenerative Medicine", "Cyborg"];
+var newTitles = [];
+var dashes = [];
+var pound = [];
+for (var i =0; i < titles.length; i++) {  
+ var pounder =  "# " + titles[i];
+ pound.push(pounder);
+   dashes.push(titles[i].replace(/\s+/g, '-'));
+ newTitles.push("[" + titles[i] + "]" + "(" + dashes[i] + ")");
+ 
+
+ 
+}
+console.log(newTitles + " " + dashes + pound);
+
+// Can be used to generate list of topics
+
+
+// [Anchored Intra-Page Links](#anchored-intrapage-links)
+// # Anchored Intrapage Links
