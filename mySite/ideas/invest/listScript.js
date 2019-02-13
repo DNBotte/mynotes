172f6cca1b9940b26dfed82060464f17227aea4b
David Botte <davidnbotte@yahoo.com>
2019-02-12 20:04:55 -0500

    add notes

--- mySite/ideas/invest/listScript.js
+++ mySite/ideas/invest/listScript.js
@@ -1,8 +1,18 @@
+// list of titles
 var titles = ["Combo Funds", "AI", "Fusion Electricity", "Additive Manufacturing", "Asteroid Mining", "Space Transportation", "Desalinization", "Invasive Species", "Lab Grown Agriculture", "3-D Transportation", "Regenerative Medicine", "Cyborg"];
+
+// title appendages get pushed here
 var newTitles = [];
+
+// links get pushed here
 var dashes = [];
+
+// destination anchors get pushed here
 var pound = [];
+
+
 for (var i =0; i < titles.length; i++) {  
+
  var pounder =  "# " + titles[i];
  pound.push(pounder);
    dashes.push("#" + titles[i].replace(/\s+/g, '-'));
