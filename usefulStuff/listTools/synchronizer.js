407a720d27ffd71055007b5a0b2536c808e125f2
David Botte <davidnbotte@yahoo.com>
2019-05-11 09:32:31 -0400

    Create synchronizer.js
    backing up old code

---
+++ usefulStuff/listTools/synchronizer.js
@@ -0,0 +1,17 @@
+//Removes fields from local table that no longer exist in source table
+
+var local = [2, 4, 6, 8];
+
+var source = [1, 2, 3, 4, 5];
+
+for (var i = 0; i < local.length; i++) {
+  for (var j = 0; j < source.length; j++) {
+    if (local[i] == source[j]) {
+     local.splice(i, 1); 
+    }
+  }
+}
+
+console.log(local);
+
+//Correct result should be: [6, 8]
